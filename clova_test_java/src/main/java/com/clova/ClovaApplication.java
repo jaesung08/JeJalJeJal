0f4e08c68c17a39ca4a5bdccb332eb2c9ad52346
정소영 <so2043@naver.com>
2024-04-25 18:40:05 +0900

    Feat : clova api 연결 테스트
    clova api 연결 테스트

--- clova_test_java/src/main/java/com/clova/ClovaApplication.java
+++ clova_test_java/src/main/java/com/clova/ClovaApplication.java
@@ -14,6 +14,7 @@ import io.grpc.netty.NettyChannelBuilder;
 import io.grpc.stub.MetadataUtils;
 import io.grpc.stub.StreamObserver;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.concurrent.CountDownLatch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,17 +26,18 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class ClovaApplication {
 
     private static final Logger logger = LoggerFactory.getLogger(ClovaApplication.class);
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception, IOException {
 
         CountDownLatch latch = new CountDownLatch(1);
         ManagedChannel channel = NettyChannelBuilder
-            .forTarget("clovaspeech-gw.ncloud.com:50051")
+//            .forTarget("clovaspeech-gw.ncloud.com:50051")
+            .forTarget("clovaspeech-gw.ncloud.com/external/v1/7632/ce61c24d820f08fbc511e01ad4160c908b53484961f6e73d80a74d7ebd7daeac")
             .useTransportSecurity()
             .build();
         NestServiceGrpc.NestServiceStub client = NestServiceGrpc.newStub(channel);
         Metadata metadata = new Metadata();
         metadata.put(Metadata.Key.of("Authorization", Metadata.ASCII_STRING_MARSHALLER),
-            "Bearer fuT1OO0o9jBcEw3kFuqD9rqQ3kkYKUzBX0CnTGNP");
+            "Bearer e4bde1dd90764fdd8189813b267b204f");
         client = MetadataUtils.attachHeaders(client, metadata);
 
         StreamObserver<NestResponse> responseObserver = new StreamObserver<NestResponse>() {

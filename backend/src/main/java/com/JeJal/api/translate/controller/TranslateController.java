fd4839ea5949c701b7e9e22b7f4b5d8035c91a84
JangJaeSung <lancer98@naver.com>
2024-05-03 13:38:32 +0900

    fix: merge 충돌로 인한 경로 꼬임 해제
    - translate 폴더 경로 조정
    - clover 폴더 경로 조정

--- backend/src/main/java/com/JeJal/api/translate/controller/TranslateController.java
+++ backend/src/main/java/com/JeJal/api/translate/controller/TranslateController.java
@@ -1,8 +1,10 @@
 package com.JeJal.api.translate.controller;
 
-import com.JeJal.global.common.response.BaseResponse;
+import com.JeJal.api.translate.dto.ClovaStudioResponseDto;
 import com.JeJal.api.translate.dto.TextDto;
+import com.JeJal.api.translate.service.ClovaStudioService;
 import com.JeJal.api.translate.service.TranslateService;
+import com.JeJal.global.common.response.BaseResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -18,14 +20,30 @@ import org.springframework.web.bind.annotation.RestController;
 public class TranslateController {
 
     private final TranslateService translateService;
+    private final ClovaStudioService clovaStudioService;
 
-    @PostMapping("/")
-    @Operation(summary = "제주 방언 번역", description = "제주 방언을 표준어로 번역합니다.")
-    public ResponseEntity<BaseResponse<TextDto>> translate(@RequestBody TextDto textDto) {
+    @PostMapping("/jebert")
+    @Operation(summary = "제주 방언 번역 - jeBert", description = "제주 방언을 표준어로 번역합니다.")
+    public ResponseEntity<BaseResponse<TextDto>> translateByJebert(@RequestBody TextDto textDto) {
 
-        TextDto translatedText = translateService.translate(textDto.getText());
+        TextDto translatedText = translateService.translateByJeBert(textDto);
         return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(BaseResponse.success(200, "번역 성공", translatedText));
+            .status(HttpStatus.OK)
+            .body(BaseResponse.success(200, "jeBert 번역 성공", translatedText));
+    }
+
+    @PostMapping("/clova")
+    @Operation(summary = "제주 방언 번역 - clova", description = "제주 방언을 표준어로 번역합니다.")
+    public ResponseEntity<BaseResponse<TextDto>> translateByClova(@RequestBody TextDto textDto) {
+        ClovaStudioResponseDto clovaStudioResponseDto = clovaStudioService.translateByClova(textDto.getText());
+
+        String translatedText = clovaStudioResponseDto.getResult().getMessage().content;
+        TextDto resultTextDto = TextDto.builder()
+            .text(translatedText)
+            .build();
+
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(BaseResponse.success(200, "clova 번역 성공", resultTextDto));
     }
 }
--- backend/src/main/java/com/JeJal/api/translate/service/TranslateService.java
+++ backend/src/main/java/com/JeJal/api/translate/service/TranslateService.java
@@ -3,12 +3,8 @@ package com.JeJal.api.translate.service;
 import com.JeJal.api.translate.dto.TextDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
-
-import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor

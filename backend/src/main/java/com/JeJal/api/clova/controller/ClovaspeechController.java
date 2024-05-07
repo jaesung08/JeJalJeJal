72b58eadd5726840dc224ce1e7089cd066fd6ad3
정소영 <so2043@naver.com>
2024-05-07 10:39:04 +0900

    Feat : clova speech api 추가

--- backend/src/main/java/com/JeJal/api/clova/controller/ClovaspeechController.java
+++ backend/src/main/java/com/JeJal/api/clova/controller/ClovaspeechController.java
@@ -10,7 +10,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,10 +25,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/clova-speech")
@@ -36,14 +37,13 @@ public class ClovaspeechController {
 
     private final ClovaspeechService clovaspeechService;
     private final RestTemplate restTemplate;  // RestTemplate 주입
-    @PostMapping("/upload")
-    @Operation(summary = "clova speech 전용 테스트", description = "로컬에 저장되어 있는 음성파일 STT")
-    public ResponseEntity<BaseResponse<?>> recognizeByUpload() throws IOException {
 
-        // 음성 파일
-        File tempFile = new File("C:\\Users\\SSAFY\\Desktop\\we_test.m4a");
 
-        // JSON 데이터 처리를 위한 ObjectMapper 인스턴스 생성
+    // 핸들러에 사용할 api (file)
+    @PostMapping("/multipartfile")
+    @Operation(summary = "핸들러에 사용할 api (multipartfile)", description = "복원된 음성 파일을 STT 처리")
+    public ResponseEntity<BaseResponse<?>> recognizeByMultipartFile(MultipartFile file) throws IOException {
+        // 기존 로직 유지
         ObjectMapper objectMapper = new ObjectMapper();
 
         // 키워드 부스팅
@@ -60,7 +60,7 @@ public class ClovaspeechController {
         request.setBoostings(boostList);
 
         // clova speech api
-        String jsonResponse = clovaspeechService.recognizeByUpload(tempFile, request);
+        String jsonResponse = clovaspeechService.recognizeByUpload(file, request);
 
         try {
             JsonNode rootNode = objectMapper.readTree(jsonResponse); // 응답 JSON을 JsonNode로 변환
@@ -70,17 +70,130 @@ public class ClovaspeechController {
 
         } catch (Exception e) {
             return ResponseEntity
-                .badRequest()
-                .body(BaseResponse.error(400, "Failed to process the file: " + e.getMessage()));
+                    .badRequest()
+                    .body(BaseResponse.error(400, "Failed to process the file: " + e.getMessage()));
         }
     }
 
-    @PostMapping("/standard")
-    @Operation(summary = "clova speech(stt)와 clova ai(번역) api 연결", description = "로컬에 저장되어 있는 음성파일 STT 후 clova ai로 번역까지")
-    public ResponseEntity<BaseResponse<?>> jejuoToStandard() throws IOException {
+    // 핸들러에 사용할 api (file)
+    @PostMapping("/file")
+    @Operation(summary = "핸들러에 사용할 api (file)", description = "복원된 음성 파일을 STT 처리")
+    public ResponseEntity<BaseResponse<?>> recognizeByFile(File file) throws IOException {
+        // 기존 로직 유지
+        ObjectMapper objectMapper = new ObjectMapper();
 
-        // 음성 파일
-        File tempFile = new File("C:\\Users\\SSAFY\\Desktop\\we_test.m4a");
+        // 키워드 부스팅
+        Resource resource = new ClassPathResource("keyword/boosting.json");
+        JsonNode keywordJson = objectMapper.readTree(new File(String.valueOf(resource.getFile().toPath())));
+        String boostingWords = keywordJson.get("boostingWords").asText();
+
+        Boosting boost = new Boosting();
+        boost.setWords(boostingWords);
+        List<Boosting> boostList = new ArrayList<>();
+        boostList.add(boost);
+
+        NestRequestDTO request = new NestRequestDTO();
+        request.setBoostings(boostList);
+
+        // clova speech api
+        String jsonResponse = clovaspeechService.recognizeByFile(file, request);
+
+        try {
+            JsonNode rootNode = objectMapper.readTree(jsonResponse); // 응답 JSON을 JsonNode로 변환
+            String textContent = rootNode.get("text").asText(); // 'text' 필드의 값을 추출
+
+            return ResponseEntity.ok(BaseResponse.success(200, "clova speech api 통신완료", textContent));
+
+        } catch (Exception e) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(BaseResponse.error(400, "Failed to process the file: " + e.getMessage()));
+        }
+    }
+
+    // 핸들러에 사용할 api (byte[])
+    @PostMapping(value = "/byte", consumes = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    @Operation(summary = "핸들러에 사용할 api (byte[])", description = "복원된 음성 파일을 STT 처리")
+    public ResponseEntity<BaseResponse<?>> recognizeBybyte(@RequestBody byte[] fileData) {
+        File tempFile = null;
+        try {
+            tempFile = File.createTempFile("upload-", ".m4a", new File("temp"));
+            try (OutputStream os = new FileOutputStream(tempFile)) {
+                os.write(fileData);
+            }
+
+            String response = sendFileToClovaSpeech(tempFile);
+            return ResponseEntity.ok(BaseResponse.success(200, "File processed successfully", response));
+        } catch (Exception e) {
+            if (tempFile != null) tempFile.delete();
+            return ResponseEntity.badRequest().body(BaseResponse.error(400, "Failed to process the file: " + e.getMessage()));
+        } finally {
+            if (tempFile != null) tempFile.delete();
+        }
+    }
+    private String sendFileToClovaSpeech(File file) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Resource resource = new ClassPathResource("keyword/boosting.json");
+        JsonNode keywordJson = objectMapper.readTree(resource.getFile());
+        String boostingWords = keywordJson.get("boostingWords").asText();
+
+        Boosting boost = new Boosting();
+        boost.setWords(boostingWords);
+        List<Boosting> boostList = new ArrayList<>();
+        boostList.add(boost);
+
+        NestRequestDTO request = new NestRequestDTO();
+        request.setBoostings(boostList);
+
+        String jsonResponse = clovaspeechService.recognizeByFile(file, request);
+        JsonNode rootNode = objectMapper.readTree(jsonResponse);
+        return rootNode.get("text").asText();
+    }
+
+
+    // 스웨거에서 파일 업로드해서 테스트 해보는 곳
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "스웨거 clova speech 전용 테스트", description = "외부에서 업로드된 음성 파일을 STT 처리")
+    public ResponseEntity<BaseResponse<?>> recognizeByUpload(
+            @RequestParam("file") MultipartFile file) throws IOException {
+        // 기존 로직 유지
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        // 키워드 부스팅
+        Resource resource = new ClassPathResource("keyword/boosting.json");
+        JsonNode keywordJson = objectMapper.readTree(new File(String.valueOf(resource.getFile().toPath())));
+        String boostingWords = keywordJson.get("boostingWords").asText();
+
+        Boosting boost = new Boosting();
+        boost.setWords(boostingWords);
+        List<Boosting> boostList = new ArrayList<>();
+        boostList.add(boost);
+
+        NestRequestDTO request = new NestRequestDTO();
+        request.setBoostings(boostList);
+
+        // clova speech api
+        String jsonResponse = clovaspeechService.recognizeByUpload(file, request);
+
+        try {
+            JsonNode rootNode = objectMapper.readTree(jsonResponse); // 응답 JSON을 JsonNode로 변환
+            String textContent = rootNode.get("text").asText(); // 'text' 필드의 값을 추출
+
+            return ResponseEntity.ok(BaseResponse.success(200, "clova speech api 통신완료", textContent));
+
+        } catch (Exception e) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(BaseResponse.error(400, "Failed to process the file: " + e.getMessage()));
+        }
+    }
+
+
+    @PostMapping("/standard")
+    @Operation(summary = "clova speech(stt)와 clova ai(번역) api 연결", description = "외부에서 업로드된 음성 파일을 STT 후 clova ai로 번역")
+    public ResponseEntity<BaseResponse<?>> jejuoToStandard(@RequestParam("filePath") String filePath) throws IOException {
+
+        File file = new File(filePath); // 파일 경로를 기반으로 File 객체 생성
 
         // JSON 데이터 처리를 위한 ObjectMapper 인스턴스 생성
         ObjectMapper objectMapper = new ObjectMapper();
@@ -99,7 +212,7 @@ public class ClovaspeechController {
         request.setBoostings(boostList);
 
         // clova speech api
-        String jsonResponse = clovaspeechService.recognizeByUpload(tempFile, request);
+        String jsonResponse = clovaspeechService.recognizeByFile(file, request);
 
         try {
             JsonNode rootNode = objectMapper.readTree(jsonResponse); // 응답 JSON을 JsonNode로 변환
@@ -115,10 +228,10 @@ public class ClovaspeechController {
 
             String translateUrl = "http://localhost:8000/api/translate/clova";  // TranslateController의 URL
             ResponseEntity<BaseResponse<?>> response = restTemplate.exchange(
-                translateUrl,
-                HttpMethod.POST,
-                requestEntity,
-                new ParameterizedTypeReference<BaseResponse<?>>() {}); // 제네릭 타입 정확히 처리
+                    translateUrl,
+                    HttpMethod.POST,
+                    requestEntity,
+                    new ParameterizedTypeReference<BaseResponse<?>>() {}); // 제네릭 타입 정확히 처리
 
             System.out.println("표준어");
             System.out.println(response.getBody().getData());
@@ -127,8 +240,8 @@ public class ClovaspeechController {
 
         } catch (Exception e) {
             return ResponseEntity
-                .badRequest()
-                .body(BaseResponse.error(400, "Failed to process the file: " + e.getMessage()));
+                    .badRequest()
+                    .body(BaseResponse.error(400, "Failed to process the file: " + e.getMessage()));
         }
     }
 }

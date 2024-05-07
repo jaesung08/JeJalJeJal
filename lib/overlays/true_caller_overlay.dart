81fa0561aa9659f22cea7a45d298c8d03bb85839
박중현 <wndgus0712@gmail.com>
2024-05-07 20:26:15 +0900

    feat: overlay.dart

--- lib/overlays/true_caller_overlay.dart
+++ lib/overlays/true_caller_overlay.dart
@@ -1,4 +1,3 @@
-import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter_overlay_window/flutter_overlay_window.dart';
 import 'package:jejal_project/databases/database.dart';
@@ -6,38 +5,36 @@ import 'package:jejal_project/overlays/tangerine_icon.dart';
 import 'package:web_socket_channel/src/channel.dart';
 
 class TrueCallerOverlay extends StatefulWidget {
-  // 클래스의 인스턴스를 생성할 때 사용할 수 있는 선택적인 키를 허용
-  // 이 키를 사용하여 위젯을 고유하게 식별 가능
   const TrueCallerOverlay({Key? key, required WebSocketChannel channel, required JejalDatabase database}) : super(key: key);
 
   @override
-  State<TrueCallerOverlay> createState() => _TrueCallerOverlayState();
+  _TrueCallerOverlayState createState() => _TrueCallerOverlayState();
 }
 
 class _TrueCallerOverlayState extends State<TrueCallerOverlay> {
-  bool showIcon = true; // Icon 표시 여부를 나타내는 변수
-  bool showBox = false; // 상자 표시 여부를 나타내는 변수
+  bool showIcon = true;
+  bool showBox = false;
 
   @override
   Widget build(BuildContext context) {
     return Material(
-      color: Colors.black87, // 배경색을 설정
-      child: Stack( // 여러 위젯을 겹쳐서 표시하기 위한 Stack 위젯
+      color: Colors.black87,
+      child: Stack(
         children: [
           Positioned(
-            top: 5.0, // 원하는 Y 위치
-            right: 20.0, // 원하는 X 위치
-            child: GestureDetector( // 사용자 동작 감지를 위한 GestureDetector 위젯
+            top: 5.0,
+            right: 20.0,
+            child: GestureDetector(
               onTap: () {
                 setState(() {
-                  showBox = !showBox; // 상자 표시 여부를 반전
-                  showIcon = !showIcon; // 아이콘 표시 여부를 반전
+                  showBox = !showBox;
+                  showIcon = !showIcon;
                 });
               },
-              child: Column( // 세로로 배치되는 위젯 그룹
+              child: Column(
                 children: [
-                  if (showIcon) TangerineIcon(), // showIcon이 true인 경우에만 아이콘 표시
-                  if (showBox) _buildBox(), // showBox가 true인 경우에만 상자 표시
+                  if (showIcon) TangerineIcon(),
+                  if (showBox) _buildBox(),
                 ],
               ),
             ),
@@ -47,10 +44,10 @@ class _TrueCallerOverlayState extends State<TrueCallerOverlay> {
     );
   }
 
-  Widget _buildBox() { // 상자를 생성하는 함수
+  Widget _buildBox() {
     return Container(
-      margin: const EdgeInsets.only(top: 10.0), // 위쪽 마진 설정
-      padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 16.0), // 내부 패딩 설정
+      margin: const EdgeInsets.only(top: 10.0),
+      padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 16.0),
       width: 300.0,
       height: 480.0,
       decoration: BoxDecoration(
@@ -59,7 +56,7 @@ class _TrueCallerOverlayState extends State<TrueCallerOverlay> {
       ),
       child: SingleChildScrollView(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start, // 왼쪽 정렬
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
               "실시간 통역 중",
@@ -90,5 +87,4 @@ class _TrueCallerOverlayState extends State<TrueCallerOverlay> {
       ),
     );
   }
-
 }

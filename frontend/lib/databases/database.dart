99ee08896ec6c37f2570abc776270d943b29e329
조성호 <whtjdgh510@naver.com>
2024-05-08 11:33:34 +0900

    feat: Change functions in database.dart

--- frontend/lib/databases/database.dart
+++ frontend/lib/databases/database.dart
@@ -13,55 +13,52 @@ class Conversations extends Table {
   TextColumn get phoneNumber => text()();
   TextColumn get name => text()();
   DateTimeColumn get date => dateTime()();
-  TextColumn get recordingFilePath => text()();
+
+  @override
+  Set<Column> get primaryKey => {id};
 }
 
-class JejuTexts extends Table {
+class TextEntries extends Table {
   IntColumn get id => integer().autoIncrement()();
   IntColumn get conversationId => integer().references(Conversations, #id)();
   TextColumn get jejuText => text()();
-  DateTimeColumn get timestamp => dateTime()();
-}
-
-class TranslatedTexts extends Table {
-  IntColumn get id => integer().autoIncrement()();
-  // conversationId 를 외래키로 사용하여 Conversations 테이블과 연결
-  IntColumn get conversationId => integer().references(Conversations, #id)();
   TextColumn get translatedText => text()();
   DateTimeColumn get timestamp => dateTime()();
+
+  @override
+  Set<Column> get primaryKey => {id};
 }
 
-@DriftDatabase(tables: [Conversations, JejuTexts, TranslatedTexts])
+@DriftDatabase(tables: [Conversations, TextEntries])
 class JejalDatabase extends _$JejalDatabase {
   JejalDatabase() : super(_openConnection());
 
   @override
   int get schemaVersion => 1;
 
+  // 모든 대화를 비동기적으로 가져옴(조회) - 통화 녹음 목록에 사용 - 통화 녹음 목록에 사용
   Future<List<Conversation>> getAllConversations() => select(conversations).get();
+
+  // 모든 대화를 실시간으로 감시 & 변경사항을 업데이트하는 스트림을 반환 - 통화 녹음 목록에 사용
   Stream<List<Conversation>> watchAllConversations() => select(conversations).watch();
+
+  // 대화를 데이터베이스에 저장
   Future<int> insertConversation(ConversationsCompanion conversation) => into(conversations).insert(conversation);
 
-  // JejuTexts 테이블에 데이터 삽입
-  Future<int> insertJejuText(JejuTextsCompanion jejuText) => into(jejuTexts).insert(jejuText);
+  // Texts 테이블에 데이터 삽입
+  Future<int> insertText(TextEntriesCompanion text) => into(textEntries).insert(text);
 
-  // TranslatedTexts 테이블에 데이터 삽입
-  Future<int> insertTranslatedText(TranslatedTextsCompanion translatedText) => into(translatedTexts).insert(translatedText);
-
-  // 특정 conversationId에 해당하는 모든 jejuTexts 데이터 조회
-  Future<List<JejuText>> getJejuTextsByConversationId(int conversationId) {
-    return (select(jejuTexts)..where((tbl) => tbl.conversationId.equals(conversationId))).get();
-  }
-
-  // 특정 conversationId에 해당하는 모든 TranslatedTexts 데이터 조회
-  Future<List<TranslatedText>> getTranslatedTextsByConversationId(int conversationId) {
-    return (select(translatedTexts)..where((tbl) => tbl.conversationId.equals(conversationId))).get();
-  }
-
-  // 특정 대화를 클릭했을 때 conversationId 조회
+  // 특정 대화를 클릭했을 때 conversationId 조회 - 특정 녹음 대화를 선택할 때 사용
   Future<Conversation> getConversationById(int id) {
     return (select(conversations)..where((tbl) => tbl.id.equals(id))).getSingle();
   }
+
+  // 특정 conversationId에 해당하는 모든 Texts 데이터 조회
+  Future<List<TextEntry>> getTextsByConversationId(int conversationId) {
+    return (select(textEntries)..where((tbl) => tbl.conversationId.equals(conversationId))).get();
+  }
+
+
 }
 
 LazyDatabase _openConnection() {

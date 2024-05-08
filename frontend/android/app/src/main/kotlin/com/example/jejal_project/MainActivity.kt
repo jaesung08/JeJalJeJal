71825ac2781012bb559a2210add4dc2bb1880625
hyunji <hinngreen@naver.com>
2024-05-08 16:18:56 +0900

    fix-fe

--- frontend/android/app/src/main/kotlin/com/example/jejal_project/MainActivity.kt
+++ frontend/android/app/src/main/kotlin/com/example/jejal_project/MainActivity.kt
@@ -1,21 +1,12 @@
-<<<<<<< HEAD
 package com.example.jejal_project
 
 import android.content.Intent
 import android.net.Uri
-=======
-
-package com.example.jejal_project
-
-import android.content.Intent
-import android.os.Bundle
->>>>>>> 6f09919dac175feb3bd4634f1fde1deb05cd503b
 import android.os.Build
 import android.provider.Settings
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
-<<<<<<< HEAD
 import com.example.jejal_project.OverlayPermissionActivity
 
 class MainActivity : FlutterActivity() {
@@ -26,28 +17,6 @@ class MainActivity : FlutterActivity() {
         MethodChannel(
             flutterEngine.dartExecutor.binaryMessenger,
             CHANNEL
-=======
-
-class MainActivity : FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // 포그라운드 서비스를 자동으로 시작
-        val intent = Intent(this, YourForegroundService::class.java)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            startForegroundService(intent)
-        } else {
-            startService(intent)
-        }
-    }
-
-    // overlay_permission"이라는 채널을 만들고, 이 채널을 통해 플러터 측에서 오버레이 권한을 요청
-    ove                     rride fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-        MethodChannel(
-            flutterEngine.dartExecutor.binaryMessenger,
-            "overlay_permission"
->>>>>>> 6f09919dac175feb3bd4634f1fde1deb05cd503b
         ).setMethodCallHandler { call, result ->
             if (call.method == "requestOverlayPermission") {
                 requestOverlayPermission(result)
@@ -84,8 +53,4 @@ class MainActivity : FlutterActivity() {
             }
         }
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 6f09919dac175feb3bd4634f1fde1deb05cd503b

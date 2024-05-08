e226da92a3e648cf1cc2a0d5c6d3e5ec84afae54
조성호 <whtjdgh510@naver.com>
2024-05-08 16:12:09 +0900

    feat: add overlayUI

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:file_picker/file_picker.dart';
+import 'package:flutter_overlay_window/flutter_overlay_window.dart';
 import 'package:jejal_project/databases/database.dart';
 import 'package:jejal_project/services/set_stream.dart';
 import 'package:permission_handler/permission_handler.dart';
@@ -47,18 +48,6 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   @override
-  void initState() {
-    super.initState();
-    _initializeApp();
-  }
-
-  Future<void> _initializeApp() async {
-    await _requestPermissions();
-    await requestOverlayPermission();
-    setStream();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -66,6 +55,42 @@ class _MyAppState extends State<MyApp> {
       home: HomePage(database: widget.database),
     );
   }
+
+  @override
+  void initState() {
+    super.initState();
+    _requestPermissions();
+    _showOverlay();
+    _initializeApp();
+  }
+
+  Future<void> _initializeApp() async {
+    // await _requestPermissions();
+    // await _showOverlay();
+    await requestOverlayPermission();
+    setStream();
+  }
+
+  // 오버레이 실행을 위한 메서드
+  Future<void> _showOverlay() async {
+    // 오버레이가 이미 활성화되어 있는지 확인
+    if (await FlutterOverlayWindow.isActive()) return;
+
+    // 오버레이 표시
+    // await FlutterOverlayWindow.showOverlay(
+    //   enableDrag: true,
+    //   overlayTitle: "제주 방언 번역기",
+    //   overlayContent: 'Overlay Enabled',
+    //   flag: OverlayFlag.defaultFlag,
+    //   // visibility 파라미터를 삭제하거나 대체함
+    //   positionGravity: PositionGravity.auto,
+    //   height: (MediaQueryData.fromWindow(WidgetsBinding.instance.window).size.height * 1.6).toInt(),
+    //   // width: WindowSize.matchParent,
+    //   width: (MediaQueryData.fromWindow(WidgetsBinding.instance.window).size.width * 2.2).toInt(),
+    //   startPosition: const OverlayPosition(0, 20),
+    // );
+  }
+
 
   Future<void> _requestPermissions() async {
     // 통화, 저장소, 백그라운드 권한 요청

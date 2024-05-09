ca7e792513b3a9fd367b6d00e55bcf21b5727505
hyunji <hinngreen@naver.com>
2024-05-09 19:25:43 +0900

    feat-fe: 위젯 구현

--- frontend/lib/overlays/true_caller_overlay.dart
+++ frontend/lib/overlays/true_caller_overlay.dart
@@ -1,4 +1,3 @@
-// true_caller_overlay.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_overlay_window/flutter_overlay_window.dart';
 import 'package:jejal_project/services/translation_service.dart';
@@ -15,8 +14,6 @@ class TrueCallerOverlay extends StatefulWidget {
 }
 
 class _TrueCallerOverlayState extends State<TrueCallerOverlay> {
-  bool showIcon = true;
-  bool showBox = false;
   List<Map<String, String>> translationPairs = [];
 
   @override
@@ -37,56 +34,54 @@ class _TrueCallerOverlayState extends State<TrueCallerOverlay> {
   Widget build(BuildContext context) {
     return Material(
       color: Colors.black87,
+      borderRadius: BorderRadius.circular(12.0),
       child: Stack(
         children: [
           Positioned(
             top: 0.0,
             right: 10.0,
-            child: GestureDetector(
-              onTap: () {
-                setState(() {
-                  showBox = !showBox;
-                  showIcon = !showIcon;
-                });
+            child: IconButton(
+              onPressed: () async {
+                await FlutterOverlayWindow.closeOverlay();
               },
-              onLongPress: () {
-                FlutterOverlayWindow.closeOverlay();
-              },
-              child: Column(
-                children: [
-                  if (showIcon) TangerineIcon(),
-                  if (showBox) _buildBox(),
-                ],
+              icon: const Icon(
+                Icons.close,
+                color: Colors.white,
               ),
             ),
           ),
+          _buildBox(),
         ],
       ),
     );
   }
 
   Widget _buildBox() {
-    return Container(
-      margin: const EdgeInsets.only(top: 10.0),
-      padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 16.0),
-      width: 340.0,
-      height: 480.0,
-      decoration: BoxDecoration(
-        color: Colors.orangeAccent.shade100,
-        borderRadius: BorderRadius.circular(12.0),
-      ),
-      child: SingleChildScrollView(
-        reverse: true,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "실시간 통역 중",
-              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-            ),
-            Divider(),
-            ...translationPairs.map((pair) => _buildTranslationPair(pair['jejuText']!, pair['translatedText']!)).toList(),
-          ],
+    return Positioned(
+      top: 40.0, // 원하는 위치 조정 가능
+      right: 10.0, // 원하는 위치 조정 가능
+      child: Container(
+        margin: const EdgeInsets.only(top: 10.0),
+        padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 16.0),
+        width: 340.0,
+        height: 250.0,
+        decoration: BoxDecoration(
+          color: Colors.orangeAccent.shade100,
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+        child: SingleChildScrollView(
+          reverse: true,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "실시간 통역 중",
+                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+              ),
+              Divider(),
+              ...translationPairs.map((pair) => _buildTranslationPair(pair['jejuText']!, pair['translatedText']!)).toList(),
+            ],
+          ),
         ),
       ),
     );

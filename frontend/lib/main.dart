b64fd9232d0d8ee9a5f3cf73cf9017c809bf1854
whtjdgh510 <whtjdgh510@naver.com>
2024-05-12 15:48:51 +0900

    fix: textbox logic

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -9,19 +9,23 @@ import 'package:flutter/services.dart';
 import 'package:jejal_project/overlays/true_caller_overlay.dart';
 import 'package:jejal_project/home_page.dart';
 
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final databaseService = DatabaseService();
 
-  // 앱 실행 시 MyApp 위젯 실행
+  await _requestPermissions();
+  await _showOverlay();
+  await requestOverlayPermission();
+  await setStream();
+  initPhoneStateListener();
+
   runApp(MyApp(databaseService: databaseService));
 }
 
 @pragma("vm:entry-point")
 void overlayMain() {
   WidgetsFlutterBinding.ensureInitialized();
-    runApp(
+  runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: TrueCallerOverlay(),
@@ -38,7 +42,7 @@ class MyApp extends StatefulWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-          fontFamily: "Pretendard"
+        fontFamily: "Pretendard",
       ),
       home: HomePage(databaseService: databaseService),
     );
@@ -53,7 +57,6 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      //메인페이지 home_page.dart에 구현
       home: HomePage(databaseService: DatabaseService()),
     );
   }
@@ -61,90 +64,57 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
-    _requestPermissions();
-    _showOverlay();
-    _initializeApp();
   }
+}
 
-  Future<void> _initializeApp() async {
-    // await _requestPermissions();
-    // await _showOverlay();
-    await requestOverlayPermission();
-    setStream();
+Future<void> _requestPermissions() async {
+  await _requestCallPermission();
+  await _requestContactPermission();
+  await _requestStoragePermission();
+  await _requestBackgroundPermission();
+  await _requestSystemAlertWindowPermission();
+}
+
+Future<void> _requestStoragePermission() async {
+  await Permission.storage.request();
+}
+
+Future<void> _requestCallPermission() async {
+  await Permission.phone.request();
+}
+
+Future<void> _requestBackgroundPermission() async {
+  await Permission.backgroundRefresh.request();
+}
+
+Future<void> _requestSystemAlertWindowPermission() async {
+  if (!await Permission.systemAlertWindow.isGranted) {
+    await Permission.systemAlertWindow.request();
   }
+}
 
-  // 오버레이 실행을 위한 메서드
-  Future<void> _showOverlay() async {
-    // 오버레이가 이미 활성화되어 있는지 확인
-    if (await FlutterOverlayWindow.isActive()) return;
-
-    // 오버레이 표시
-    // await FlutterOverlayWindow.showOverlay(
-    //   enableDrag: true,
-    //   overlayTitle: "제주 방언 번역기",
-    //   overlayContent: 'Overlay Enabled',
-    //   flag: OverlayFlag.defaultFlag,
-    //   // visibility 파라미터를 삭제하거나 대체함
-    //   positionGravity: PositionGravity.auto,
-    //   height: (MediaQueryData.fromWindow(WidgetsBinding.instance.window).size.height * 1.6).toInt(),
-    //   // width: WindowSize.matchParent,
-    //   width: (MediaQueryData.fromWindow(WidgetsBinding.instance.window).size.width * 2.2).toInt(),
-    //   startPosition: const OverlayPosition(0, 20),
-    // );
+Future<void> _requestContactPermission() async {
+  try {
+    await Permission.contacts.request();
+  } catch (e) {
+    print('Error requesting contact permission: $e');
   }
+}
 
+Future<void> _showOverlay() async {
+  if (await FlutterOverlayWindow.isActive()) return;
+}
 
-  Future<void> _requestPermissions() async {
-    // 통화, 저장소, 백그라운드 권한 요청
-    await _requestCallPermission();
-    await _requestContactPermission(); // 연락처 권한 요청 추가
-    await _requestStoragePermission();
-    await _requestBackgroundPermission();
-    await _requestSystemAlertWindowPermission();
-  }
-
-  Future<void> _requestStoragePermission() async {
-    // 저장소 권한 요청
-    await Permission.storage.request();
-  }
-
-  Future<void> _requestCallPermission() async {
-    // 통화 권한 요청
-    await Permission.phone.request();
-  }
-
-  Future<void> _requestBackgroundPermission() async {
-    // 백그라운드 권한 요청
-    await Permission.backgroundRefresh.request();
-  }
-
-  Future<void> _requestSystemAlertWindowPermission() async {
-    if (!await Permission.systemAlertWindow.isGranted) {
-      // 권한이 허용되지 않은 경우 권한 요청
-      await Permission.systemAlertWindow.request();
+Future<void> requestOverlayPermission() async {
+  const platform = MethodChannel('overlay_permission');
+  try {
+    final result = await platform.invokeMethod('requestOverlayPermission');
+    if (result) {
+      print('Overlay permission granted');
+    } else {
+      print('Overlay permission not granted');
     }
-  }
-
-  // 연락처 권한 추가
-  Future<void> _requestContactPermission() async {
-    try {
-      await Permission.contacts.request();
-    } catch (e) {
-      print('Error requesting contact permission: $e');
-    }
-  }
-
-  Future<void> requestOverlayPermission() async {
-    const platform = MethodChannel('overlay_permission');
-    try {
-      final result = await platform.invokeMethod('requestOverlayPermission');
-      if (result) {
-        print('Overlay permission granted');
-      } else {
-        print('Overlay permission not granted');
-      }
-    } on PlatformException catch (e) {
-      print('Error: ${e.message}');
-    }
+  } on PlatformException catch (e) {
+    print('Error: ${e.message}');
   }
 }
--- frontend/lib/services/set_stream.dart
+++ frontend/lib/services/set_stream.dart
@@ -1,3 +1,5 @@
+// lib/services/set_stream.dart
+
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
@@ -14,19 +16,21 @@ import '../models/receive_message_model.dart';
 import '../models/send_message_model.dart';
 import 'database_service.dart';
 
-void setStream() async {
+WebSocketChannel? ws;
+String? phoneNumber;
+Directory? recordDirectory;
+Directory? testDirectory;
+String? androidId;
+const String recordDirectoryPath = "/storage/emulated/0/Recordings/Call";
+File? targetFile;
+File? testFile;
+Timer? timer;
+int offset = 0;
+PhoneState? phoneStatus;
+int? conversationId;
+
+Future<void> setStream() async {
   const String testfilePath = "/storage/emulated/0/Recordings/test";
-  WebSocketChannel? ws;
-  late String phoneNumber;
-  late Directory recordDirectory;
-  late Directory testDirectory;
-  late String androidId;
-  const String recordDirectoryPath = "/storage/emulated/0/Recordings/Call";
-  File? targetFile;
-  File? testFile;
-  Timer? timer;
-  int offset = 0;
-  PhoneState phoneStatus = PhoneState.nothing();
   DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
   AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
 
@@ -34,46 +38,27 @@ void setStream() async {
 
   recordDirectory = Directory(recordDirectoryPath);
   testDirectory = Directory(testfilePath);
+}
 
+void initPhoneStateListener() {
   PhoneState.stream.listen((event) async {
     phoneStatus = event as PhoneState;
     if (event.status == PhoneStateStatus.CALL_INCOMING) {
       print("Incoming call detected.");
-    }
-
-    else if (event.status == PhoneStateStatus.CALL_STARTED) {
-      //전화 시작되면 위젯 띄우기
-      //지금 실행 안되고 있음
-      // if (!await FlutterOverlayWindow.isActive()) {
-      //   await FlutterOverlayWindow.showOverlay(
-      //     enableDrag: true,
-      //     overlayTitle: "Overlay Title",
-      //     overlayContent: 'Overlay Content',
-      //     flag: OverlayFlag.defaultFlag,
-      //     visibility: NotificationVisibility.visibilityPublic,
-      //     positionGravity: PositionGravity.auto,
-      //     height: WindowSize.matchParent,
-      //     width: WindowSize.matchParent,
-      //   );
-      // }
-
+    } else if (event.status == PhoneStateStatus.CALL_STARTED) {
       print("Call started.");
 
-      //전화번호 받아오기
-      phoneNumber = phoneStatus.number.toString();
-      //핸드폰 주소록에 접근해서 받아온 전화번호와 일치하는 사람의 이름을 name이라는 변수에 저장
-      List<Contact>? contacts = await ContactsService.getContactsForPhone(phoneNumber);
+      phoneNumber = phoneStatus!.number.toString();
+      List<Contact>? contacts = await ContactsService.getContactsForPhone(phoneNumber!);
       String? name;
 
       if (contacts != null && contacts.isNotEmpty) {
-        // 첫 번째 연락처의 이름을 사용합니다.
         name = contacts.first.displayName;
       } else {
-        name = "제주도민"; // 연락처를 찾지 못한 경우 null로 설정
+        name = "제주도민";
       }
 
-      // 새로운 대화 생성 및 conversation_id 가져오기
-      int conversationId = await DatabaseService.instance.insertConversation(phoneNumber, name!);
+      conversationId = await DatabaseService.instance.insertConversation(phoneNumber!, name!);
       print("전화온 번호: $phoneNumber");
       print("상대방 이름: $name");
 
@@ -83,12 +68,11 @@ void setStream() async {
 
       var startMessage = SendMessageModel(
         state: 0,
-        androidId: androidId,
+        androidId: androidId!,
       );
       ws?.sink.add(jsonEncode(startMessage));
 
-
-      var temp = await recentFile(recordDirectory);
+      var temp = await recentFile(recordDirectory!);
       targetFile = temp is FileSystemEntity ? temp as File : null;
 
       offset = 0;
@@ -96,7 +80,6 @@ void setStream() async {
         print("파일 찾음");
         print(targetFile?.path);
         timer = Timer.periodic(const Duration(seconds: 6), (timer) async {
-
           Uint8List entireBytes = targetFile!.readAsBytesSync();
           var nextOffset = entireBytes.length;
 
@@ -106,31 +89,21 @@ void setStream() async {
 
           ws?.sink.add(splittedBytes);
         });
-      }
-
-      else {
+      } else {
         print("파일 못찾음");
         ws?.sink.close();
       }
 
-      // 검사 결과 수신
       ws?.stream.listen((msg) async {
         if (msg != null) {
-          ReceiveMessageModel receivedResult =
-          ReceiveMessageModel.fromJson(jsonDecode(msg));
-          receivedResult.conversationId = conversationId; // conversationId 설정
+          ReceiveMessageModel receivedResult = ReceiveMessageModel.fromJson(jsonDecode(msg));
+          receivedResult.conversationId = conversationId;
           FlutterOverlayWindow.shareData(msg);
-          await DatabaseService.instance.insertMessage(receivedResult, conversationId); // 메시지 저장
+          await DatabaseService.instance.insertMessage(receivedResult, conversationId!);
         }
       });
-
     } else if (event.status == PhoneStateStatus.CALL_ENDED) {
       print("Call ended.");
-
-      //통화 종료되면 위젯 끄기
-      //됐다가 안됐다가 함
-      //UX 상으로 없애는게 나을 수도 있을 듯
-      // FlutterOverlayWindow.closeOverlay();
 
       timer?.cancel();
       Uint8List entireBytes = targetFile!.readAsBytesSync();
@@ -140,17 +113,13 @@ void setStream() async {
 
       print("마지막 데이터");
       print(splittedBytes);
-      // var encode = base64.encode(splittedBytes);
-      // print(encode);
 
-      // 마지막 데이터 전송
       ws?.sink.add(splittedBytes);
 
-      //종료 메세지 전송
       var endMessage = SendMessageModel(
         state: 1,
-        androidId: androidId,
-        phoneNumber: phoneNumber,
+        androidId: androidId!,
+        phoneNumber: phoneNumber!,
       );
       ws?.sink.add(jsonEncode(endMessage));
     }
